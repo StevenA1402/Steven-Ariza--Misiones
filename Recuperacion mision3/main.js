b451e2595f6a6e06d78c17bbe5003a399dbc60af
StevenA1402 <gonzalezelvert@gmail.com>
2023-03-29 18:08:47 -0500

    recuperasao

--- Recuperacion mision3/main.js
+++ Recuperacion mision3/main.js
@@ -3,14 +3,7 @@ const email = document.getElementById("email");
 const usuario = document.getElementById("usuario");
 const emailCambio = document.getElementById("emailCambio");
 const userCambio = document.getElementById("userCambio");
-
-
-
 const array = [];
-
-// function setLocal(event){
-//     localStorage.setItem('id',  event.target.id)
-// }
 
 class Persona {
   constructor(email, usuario) {
@@ -19,41 +12,40 @@ class Persona {
   }
 }
 
-// login.addEventListener('click',()=>{
-//     // if(email.value != '' && usuario.value != ''){
-//     //     const nuevoUsuario = new Persona(email.value,usuario.value)
-//     //     array.push(nuevoUsuario)
-//     //     console.log(nuevoUsuario)
-//     //     localStorage.setItem("productos", JSON.stringify(array));
-
-//     //     email.value=''
-//     //     usuario .value=''
-//     // }else{
-//     //     alert("Faltan Campos")
-//     // }
-
-// })
-
-const main = async () => {
+const datos = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
-  console.log(data);
-  login.addEventListener ('click', () => {
-    for (let index = 0; index < data.length; index++) {
-        const emailData = data[index].email;
-        const usuarioData = data[index].username;
-        console.log(emailData);
-        console.log(usuarioData);
-        if(emailData == email.value && usuarioData == usuario.value){
-            location.href='inicio.html'
-        }else if(emailData != email.value ){
-            emailCambio.innerHTML = `<h2>Email incorrecto, Por favor ingrese nuevamente</h2>`
-        }else if(usuarioData != usuario.value){
-            userCambio.innerHTML = `<h2>Usuario incorrecto, Por favor ingrese nuevamente</h2>`
-           
-        }
+  localStorage.setItem('data', JSON.stringify(data));
+}
+const main = () => {
+  const data = JSON.parse(localStorage.getItem('data'));
+  let usuarioCorrecto = false;
+  let emailCorrecto = false;
+  for (let index = 0; index < data.length; index++) {
+    const emailData = data[index].email;
+    const usuarioData = data[index].username;
+    console.log(emailData);
+    console.log(usuarioData);
+    if (usuarioData == usuario.value) {
+      usuarioCorrecto = true;
+      if (emailData == email.value) {
+        emailCorrecto = true;
+        location.href = 'inicio.html'
+        break;
       }
-  })    
+    }
+  }
+  if (!usuarioCorrecto) {
+    usuario.value = '';
+    userCambio.innerHTML = `<h2>Usuario incorrecto, Por favor ingrese nuevamente</h2>`
+  }
+  if (usuarioCorrecto && !emailCorrecto) {
+    email.value = '';
+    emailCambio.innerHTML = `<h2>Email incorrecto, Por favor ingrese nuevamente</h2>`
+  }
 };
 
-main();
+login.addEventListener('click', async () => {
+  await datos();
+  main();
+});
